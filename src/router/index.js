5543efa27f238f40d5faa6300c249328b3afb15c
Ragavan Rajan <ragavanrajan@outlook.com>
2020-02-12 20:26:59 +1300

    VueApp with Firebase Authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,44 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login';
+import Meetings from '../views/Meetings';
+import Register from '../views/Register';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+    },
+
+    {
+        path: '/meetings',
+        name: 'meetings',
+        component: Meetings,
+    },
+
+    {
+        path: '/register',
+        name: 'register',
+        component: Register,
+    },
+    {
+        path: '*',
+        redirect: '/',
+    },
 ];
 
 const router = new VueRouter({
-  routes
+    routes,
 });
 
 export default router;
---
+++ src/views/Meetings.vue
@@ -0,0 +1,11 @@
+<template
+    ><div><h1>Meetings</h1></div>
+</template>
+
+<script>
+export default {
+    name: 'Meetings',
+};
+</script>
+
+<style scoped></style>
